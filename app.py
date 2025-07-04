6d51ca572322cfb16640015dd16d90e7647b4d32
jk32 <jk32@sanger.ac.uk>
2025-07-04 15:14:26 +0100

    adding

--- app.py
+++ app.py
@@ -11,3 +11,20 @@ st.write("You're scheduled for:", appointment)
 
 age = st.slider("whats ur number?", 0, 130, 1000000000000)
 st.write("my number is ", age,)
+
+
+options = st.multiselect(
+    "what is ur name?",
+    ["Aaron", "Abigail", "Adam", "Adrian", "Aiden", "Alan", "Albert", "Alexa", "Alexander", "Alexandra", "Alice", "Alicia", "Allison", "Amanda", "Amber", "Amelia", "Amy", "Andrea", "Andrew", "Angela", "Anna", "Anthony", "April", "Arthur", "Ashley", "Audrey", "Austin", "Barbara", "Benjamin", "Bethany", "Beverly", "Bianca", "Blake", "Brandon", "Brenda", "Brian", "Brittany", "Brooke", "Caleb"],
+    default=["Aaron", "Adam"],
+)
+
+st.write("You selected:", options)
+
+options = st.multiselect(
+    "what is ur name?",
+    ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez", "Hernandez", "Lopez", "Gonzalez", "Wilson", "Anderson", "Thomas", "Taylor", "Moore", "Jackson", "Martin", "Lee", "Perez", "Thompson", "White", "Harris", "Sanchez", "Clark", "Ramirez", "Lewis", "Robinson", "Walker"],
+    default=["Smith"],
+)
+
+st.write("You selected:", options)
